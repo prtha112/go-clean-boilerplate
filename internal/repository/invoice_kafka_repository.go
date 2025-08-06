2210fa40ee8e219b31a08274bcdf98bb564c0ffc
Sathabhron <thahi5_vi@hotmail.com>
2025-08-07 00:17:32 +0700

    refactor: rename InvoicePostgres to invoicePostgres for consistency in naming convention

--- internal/repository/invoice_kafka_repository.go
+++ internal/repository/invoice_kafka_repository.go
@@ -6,15 +6,15 @@ import (
 	"log"
 )
 
-type InvoicePostgres struct {
+type invoicePostgres struct {
 	db *sql.DB
 }
 
-func NewInvoicePostgres(db *sql.DB) *InvoicePostgres {
-	return &InvoicePostgres{db: db}
+func NewInvoicePostgres(db *sql.DB) *invoicePostgres {
+	return &invoicePostgres{db: db}
 }
 
-func (r *InvoicePostgres) Save(inv *domain.InvoiceKafka) error {
+func (r *invoicePostgres) Save(inv *domain.InvoiceKafka) error {
 	_, err := r.db.Exec("INSERT INTO invoices_log (id, amount) VALUES ($1, $2)", inv.ID, inv.Amount)
 	log.Printf("Saving invoice with ID: %d and Amount: %.2f", inv.ID, inv.Amount)
 	return err
